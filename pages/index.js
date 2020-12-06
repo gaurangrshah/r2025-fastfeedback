d5372b0e6eb5bc3e1809b61cf16dde2f5fb7bdfe
gaurangrshah <gaurang.r.shah@gmail.com>
2020-12-05 21:32:23 -0500

    test pr

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome <a href='https://nextjs.org'>Home</a>
+          Hello <a href='https://nextjs.org'>Nurse</a>
         </h1>
 
         <p className={styles.description}>
